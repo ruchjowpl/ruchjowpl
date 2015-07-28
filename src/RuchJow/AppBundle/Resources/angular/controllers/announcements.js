e89dda70a241cb615a7e1694fd63598678ce41ae
Jakub Paszkiewicz <j.paszkiewicz@stermedia.pl>
2015-07-28 14:50:51 +0200

    Komunikaty

--- src/RuchJow/AppBundle/Resources/angular/controllers/announcements.js
+++ src/RuchJow/AppBundle/Resources/angular/controllers/announcements.js
@@ -6,15 +6,22 @@
             $scope.status = 'loading';
 
             ruchJowRss.getFeed('announcements', true).then(function (feed) {
-                $scope.feed = feed;
+                $scope.feed = feed.slice(0, 3);
                 $scope.status = 'success';
+
             }, function () {
                 $scope.status = 'error';
             });
 
             $scope.date = function (value) {
                 return new Date(value);
-            }
+            };
+            $scope.description = function (item) {
+                var result = item.description.__cdata.split('<p>');
+                result = result[1].replace('</p>','');
+                result = result+' <a href="'+item.link+'" target="_blank">czytaj dalej</a>';
+                return result;
+            };
         }]);
 
 })(angular);
